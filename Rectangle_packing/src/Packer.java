dc79489066cccddd28ef21d5c7efa77a142feecc
LENOVO-Y700\Bence <bencetaylor@gmail.com>
2017-10-26 14:18:51 +0200

    Added read Added print Logic starting

--- Rectangle_packing/src/Packer.java
+++ Rectangle_packing/src/Packer.java
@@ -1,48 +1,88 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
 public class Packer {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		// Lista a teglalapoknak
 		ArrayList<Rectangle> rects = new ArrayList<>();
+		
+		// Lista a beolvasott adatoknak, ezeket Parse-oljuk es toltjuk be beloluk az adatokat
+		ArrayList<String> input = new ArrayList<>();
+		
+		int itemCount;
 		
 		// Container dimensions
 		int contH, contW;
 		
-		contH=3;
-		contW=5;
+		// Container
+		int[][] container;
 		
+//<<----Beolvasas------------------------------------------------------->>//
+		// FileInutStream-et atirni System.In-re!!!
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("in.txt")));
+		try{
+    		while(true){
+    			String line = br.readLine();
+    			if(line == null)
+    				break;
+    			input.add(line);
+    		}
+    		br.close();
+		}
+		catch(IOException e){
+			System.out.println(e);
+		}
+		
+		// Beallitjuk a kontener meretet
+		String[] dimStr = new String[2];
+		dimStr = input.get(0).split("\t");
+		contW = Integer.parseInt(dimStr[0]);
+		contH = Integer.parseInt(dimStr[1]);
 		// Load the container with 0-s
-		int[][] container = new int[contH][contW];
-		
+		container = new int[contH][contW];
 		for(int i = 0; i<contH; i++) {
 			for(int j=0; j<contW; j++)
 				container[i][j] = 0;
 		}
 		
-		// Init rectangles
-		rects.add(new Rectangle(2, 3, 1));
-		rects.add(new Rectangle(2, 1, 2));
-		rects.add(new Rectangle(1, 3, 3));
-		rects.add(new Rectangle(1, 4, 4));
-		rects.add(new Rectangle(3, 3, 5));
+		// Megnezzuk mennyi teglalap lesz
+		itemCount = Integer.parseInt(input.get(1));
 		
-		// Put them in 2 lists
-		ArrayList<Rectangle> rects2 = new ArrayList<>();
-		for(int i=0; i<rects.size(); i++) {
-			rects2.add(rects.get(i));
+		// Beolvassuk a teglalapokat
+		for(int i = 0; i < itemCount; i++){
+			String[] rectData = input.get(i+2).split("\t");
+			rects.add(new Rectangle(Integer.parseInt(rectData[0]), Integer.parseInt(rectData[1])));
 		}
 		
-		// Sort the list by x and y
+		// Sort the list by x
 		Collections.sort(rects, new XComparator());
-		Collections.sort(rects2, new YComparator());
 		
-		for (Rectangle r : rects2) {
+		//Kiirjuk a teglalapok ID-ját
+		for (Rectangle r : rects) {
 			System.out.println(r.id);
 		}
+//<<----Logika--------------------------------------------------------------------->>//
+		Methods m = new Methods();
+		Rectangle r = rects.get(0);
+		m.putRectToCont(r, container, 0, 0);
+		/*
+		for(int i=0; i<contW; i++){
+			for(int j=0; j<contH; j++){
+				if(container[i][j]==0){
+					
+				}
+			}
+		}
 		
+		*/
+//<<----Kiiratas------------------------------------------------------------------->>//
 		// Print the results
 		for(int i = 0; i<contH; i++) {
 			for(int j=0; j<contW; j++) {
@@ -51,17 +91,36 @@ public class Packer {
 				else
 					System.out.print(container[i][j]);
 			}
-			System.out.print("\n");
+			if(i<contH-1)
+				System.out.print("\n");
 		}
 	}
 
+	public static class Methods{
+		public void putRectToCont(Rectangle tmp, int[][] container,int x, int y){
+			for(int i=x; i<tmp.width+x; i++){
+				for(int j=y; j<tmp.height+y; j++){
+					container[i][j] = tmp.id;
+				}
+			}
+		}
+	}
+	
 	public static class Rectangle {
-		public int x, y, id;
+		public int width, height, id;
+		//Auto-inkremens amibol az id-t generaljuk
+		private static int counter=1;
 		
-		public Rectangle(int _x, int _y, int _id) {
-			x=_x;
-			y=_y;
-			id=_id;
+		public Rectangle(int _width, int _height) {
+			width=_width;
+			height=_height;
+			id = counter++;
+		}
+		
+		public void rotate(){
+			int tmp = width;
+			width = height;
+			height = tmp;
 		}
 	}
 	
@@ -69,9 +128,9 @@ public class Packer {
 		
 		@Override
 		public int compare(Rectangle r1, Rectangle r2) {
-			int res = Integer.compare(r2.x, r1.x);
+			int res = Integer.compare(r2.width, r1.width);
 			if(res == 0)
-				res = Integer.compare(r2.y, r1.y);
+				res = Integer.compare(r2.height, r1.height);
 			return res;
 		}
 	}
@@ -80,10 +139,18 @@ public class Packer {
 		
 		@Override
 		public int compare(Rectangle r1, Rectangle r2) {
-			int res = Integer.compare(r2.y, r1.y);
+			int res = Integer.compare(r2.height, r1.height);
 			if(res == 0)
-				res = Integer.compare(r2.x, r1.x);
+				res = Integer.compare(r2.width, r1.width);
 			return res;
+		}
+	}
+	
+	public static class AreaComparator implements Comparator<Rectangle>{
+		
+		@Override
+		public int compare(Rectangle r1, Rectangle r2) {
+			return Integer.compare(r2.height*r2.width, r1.height*r1.width);
 		}
 	}
 }
