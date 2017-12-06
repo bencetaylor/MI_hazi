de15a490642cc95ccb76842eb2e58f1fd0157f22
LENOVO-Y700\Bence <bencetaylor@gmail.com>
2017-12-06 21:02:09 +0100

    Pearson

--- Offering_System/src/Main.java
+++ Offering_System/src/Main.java
@@ -6,15 +6,24 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+	static int[][] inputRatings;
+	static int[][] R;
+	static int ratingCount;
+	static int usersCount;
+	static int movieCount;
+	static double[] usersAvrRating;
+	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("MI_HF03_TEST.txt")));
 		String[] inputData = br.readLine().split("\t");
-		int ratingCount = Integer.parseInt(inputData[0]);
-		int usersCount = Integer.parseInt(inputData[1]);
-		int movieCount = Integer.parseInt(inputData[2]);
+		ratingCount = Integer.parseInt(inputData[0]);
+		usersCount = Integer.parseInt(inputData[1]);
+		movieCount = Integer.parseInt(inputData[2]);
 			
-		int[][] inputRatings = new int[ratingCount][3];
+		inputRatings = new int[ratingCount][3];
+		
+		R = new int[usersCount][movieCount];
 		
 		for(int i=0; i<ratingCount; i++){
 			String[] line = br.readLine().split("\t");
@@ -22,11 +31,68 @@ public class Main {
 			inputRatings[i][1]= Integer.parseInt(line[1]);
 			inputRatings[i][2]= Integer.parseInt(line[2]);
 		}
-		
+		// R feltöltése elemekkel
 		for(int i=0; i<ratingCount; i++){
-			System.out.println(inputRatings[i][0] + "\t" + inputRatings[i][1] + "\t" + inputRatings[i][2]);
+			R[inputRatings[i][0]][inputRatings[i][1]]=inputRatings[i][2];
 		}
 		
+		usersAvrRating = new double[usersCount];
+		for(int i=0; i<usersCount; i++){
+			int Vi = 0;
+			// Számláló, hogy a felhasználó hány filmet értékelt
+			int count=0; 
+			for(int j=0; j<movieCount; j++){
+				if(R[i][j] > 0){
+					Vi += R[i][j];
+					count++;
+				}
+			}
+			usersAvrRating[i] = (double)Vi * (1.0 / (double)count);
+		}
+		
+		/*
+		// Átlagolt felh. értékelések kiírsa
+		for(int i=0; i<usersCount; i++){
+			System.out.println(usersAvrRating[i]);
+		}
+		*/
+		
+		/*
+		// R mátrix kiírása
+		for(int i=0; i<usersCount; i++){
+			for(int j=0; j<movieCount; j++){
+				System.out.print(R[i][j] + "\t");
+			}
+			System.out.print("\n");
+		}*/
+		
+		/*for(int i=0; i<ratingCount; i++){
+			System.out.println(inputRatings[i][0] + "\t" + inputRatings[i][1] + "\t" + inputRatings[i][2]);
+		}*/
+		
+		br.close();
+	}
+	
+	static double PearsonCorr(int a, int i){
+		
+		double szamlalo = 0; 
+		double nevezo = 0;
+		double x=0, y = 0;
+		for(int j=0; j<movieCount; j++){
+			szamlalo += ((double)R[a][j]-usersAvrRating[a]) * ((double)R[i][j]-usersAvrRating[i]);
+			
+			x += Squared((double)R[a][j]-usersAvrRating[a]);
+			y += Squared((double)R[i][j]-usersAvrRating[i]);
+		}
+		
+		//TODO sqrt on x*y!!
+		nevezo = x*y;
+		
+		return szamlalo/nevezo;
+	}
+	
+	static double Squared(double a){
+		return a*a;
 	}
 
 }
